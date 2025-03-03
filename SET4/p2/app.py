d8c473e34669030f326c12529a192996271f6eaf
rupesh7399 <rupesh7399@gmail.com>
2025-03-03 09:21:29 +0530

    changes in set4 p2.

--- SET4/p2/app.py
+++ SET4/p2/app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+# Route to serve the main page
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def home():
-    return render_template('index.html', message='Hello, World!')
+# AJAX endpoint that returns data
+@app.route('/get_data')
+def get_data():
+    data = {"message": "Hello from Flask!"}
+    return jsonify(data)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
